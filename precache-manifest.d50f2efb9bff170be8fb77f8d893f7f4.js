69d4532b75ab1ffe03515618a9df31f172717e4a
Irshad <irshadnu@gmail.com>
2018-12-26 23:57:23 +0530

    Updates

---
+++ precache-manifest.d50f2efb9bff170be8fb77f8d893f7f4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "56f097245c605a5d312f103dedb21bcb",
+    "url": "/react-redux-final/static/media/poster4.56f09724.jpg"
+  },
+  {
+    "revision": "3dfedef06c3a20c73c71",
+    "url": "/react-redux-final/static/css/main.a8084047.chunk.css"
+  },
+  {
+    "revision": "869d986ac0a8d157cac8",
+    "url": "/react-redux-final/static/js/1.869d986a.chunk.js"
+  },
+  {
+    "revision": "ffd4c96ab7bd4d0ff185",
+    "url": "/react-redux-final/static/js/runtime~main.ffd4c96a.js"
+  },
+  {
+    "revision": "d5f2a7f493e94ed267d41620eef143d9",
+    "url": "/react-redux-final/static/media/poster1.d5f2a7f4.jpg"
+  },
+  {
+    "revision": "81ccf144677486906fcf751450652895",
+    "url": "/react-redux-final/static/media/poster2.81ccf144.jpg"
+  },
+  {
+    "revision": "e242f0372e7bdbf2e48aa5afefc72b97",
+    "url": "/react-redux-final/static/media/poster3.e242f037.jpg"
+  },
+  {
+    "revision": "3dfedef06c3a20c73c71",
+    "url": "/react-redux-final/static/js/main.3dfedef0.chunk.js"
+  },
+  {
+    "revision": "5ef7f79461c7a8c0a95652b813fb4722",
+    "url": "/react-redux-final/static/media/poster5.5ef7f794.jpg"
+  },
+  {
+    "revision": "164b05a3cd770650dd30bc71f5407e99",
+    "url": "/react-redux-final/static/media/poster6.164b05a3.jpg"
+  },
+  {
+    "revision": "23ac494a92660204d6b8acbba20aa0e9",
+    "url": "/react-redux-final/static/media/poster7.23ac494a.jpg"
+  },
+  {
+    "revision": "ef89f612f6e3d684892d6525b7957ce4",
+    "url": "/react-redux-final/static/media/poster8.ef89f612.jpg"
+  },
+  {
+    "revision": "65d6d5002a2f98860f29d3c61599c5ac",
+    "url": "/react-redux-final/static/media/poster9.65d6d500.jpg"
+  },
+  {
+    "revision": "386d68f68adeb0cf74e817eb6c9982b1",
+    "url": "/react-redux-final/index.html"
+  }
+];
